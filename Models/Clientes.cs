59f79030178f6b648a1e6036a6dbdb1bf0f5723c
emacardona <ediaz3157@gmail.com>
2024-10-25 19:24:41 -0600

    new api

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -3,7 +3,6 @@ namespace ApiEmpresa.Models;
 public class Clientes{
 [Key]
 public Int32 id_Cliente {get;set;}
-public string? nit{get;set;}
 public string? nombres{get;set;}
 public string? apellidos{get;set;}
 public string? direccion{get;set;}
