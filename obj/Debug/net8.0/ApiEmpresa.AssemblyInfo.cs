4491b2b848646a9e55722e3a4c0ce05f5f72e40f
emacardona <ediaz3157@gmail.com>
2024-10-25 19:21:58 -0600

    net 8

--- obj/Debug/net8.0/ApiEmpresa.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiEmpresa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiEmpresa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d508550247edc1f535b72783c444cc169c9307")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiEmpresa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiEmpresa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
